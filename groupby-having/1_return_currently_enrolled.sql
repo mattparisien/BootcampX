458f05cd7154c9160c52af2a2a45da0aea664f67
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-24 19:16:51 +0000

    Add a query using grourpby

---
+++ groupby-having/1_return_currently_enrolled.sql
@@ -0,0 +1,5 @@
+SELECT students.name as student, COUNT(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+INNER JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
