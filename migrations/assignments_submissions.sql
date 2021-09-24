35e0b2788fa120bb464c3160e8053cdc309cac6b
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-24 20:59:49 +0000

    Spacing fix

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -9,7 +9,7 @@ CREATE TABLE assignments (
 
 CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER PREFERENCES assignments(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
