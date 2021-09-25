73f2fd8aa4d39bdca52ee2127eeb4ef2a4fa649c
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-25 13:52:53 +0000

    Add query for total student assistances and total student request

---
+++ 4_queries/2_total_student_ass_requests.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(assistance_requests.*) as total_assistances, students.name 
+FROM assistance_requests
+INNER JOIN students ON students.id = student_id
+WHERE NAME = 'Elliot Dickinson'
+GROUP BY students.name
