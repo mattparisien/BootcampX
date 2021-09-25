cd99831f3fe7b01442c5982cfcf7a9257d704b94
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-25 20:33:36 +0000

    Add various averaging queries

---
+++ 4_queries/3_important_data.sql
@@ -0,0 +1,7 @@
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (assistance_requests.completed_at - assistance_requests.started_at) as duration
+FROM assignments
+INNER JOIN assistance_requests ON assignment_id = assignments.id
+INNER JOIN teachers ON teachers.id = teacher_id
+INNER JOIN students ON students.id = student_id
+ORDER BY duration
+
