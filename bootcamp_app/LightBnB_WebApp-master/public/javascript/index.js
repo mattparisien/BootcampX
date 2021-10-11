70c59ee4500b5f45a35e273f20b9223f78d92a44
Matthew Parisien <matthewparisien4@gmail.com>
2021-10-11 15:26:18 +0000

    Add a click event to reserve button and retrieve id attribute on click

--- bootcamp_app/LightBnB_WebApp-master/public/javascript/index.js
+++ bootcamp_app/LightBnB_WebApp-master/public/javascript/index.js
@@ -1,6 +1,12 @@
-$(() => {
-  getAllListings().then(function( json ) {
+$(document).ready(function () {
+  getAllListings().then(function (json) {
     propertyListings.addProperties(json.properties);
-    views_manager.show('listings');
+    views_manager.show("listings");
+    //Listen for click - when clicked retrieve the node's id value
+    $btn = $(".reserve-button");
+    $btn.on("click", function () {
+      const idData = $(this).attr("id");
+      console.log(idData);
+    });
   });
 });
