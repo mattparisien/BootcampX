296dd42b9e0e74fe4e87e32f39b6d9215d1bb9fa
Matt Parisien <63109316+mattparisien@users.noreply.github.com>
2021-10-16 21:42:41 -0400

    Delete 2_queries_join directory

--- 2_queries_join/1_total_time_spent_on_ass.sql
+++
@@ -1,5 +0,0 @@
-SELECT SUM(assignment_submissions.duration) as total_duration
-FROM students
-INNER JOIN assignment_submissions 
-ON students.id = student_id
-WHERE students.name = 'Ibrahim Schimmel';
