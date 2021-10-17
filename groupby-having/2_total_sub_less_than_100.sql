c76e64ca5fe403dd62d5d0091004a1e5fb56676e
Matt Parisien <63109316+mattparisien@users.noreply.github.com>
2021-10-16 21:42:28 -0400

    Delete groupby-having directory

--- groupby-having/2_total_sub_less_than_100.sql
+++
@@ -1,6 +0,0 @@
-SELECT students.name as student, COUNT(assignment_submissions.*) as total_submissions
-FROM assignment_submissions
-INNER JOIN students ON students.id = student_id
-WHERE students.end_date IS NULL
-GROUP BY students.name
-HAVING COUNT(assignment_submissions.*) < 100;
