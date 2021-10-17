fcd429d014152fccdb456fddd69f6665c6da911f
Matthew Parisien <matthewparisien4@gmail.com>
2021-10-17 01:59:20 +0000

    Reconfigure directory structure

---
+++ LightBnB_WebApp-master/public/javascript/network.js
@@ -0,0 +1,106 @@
+function getMyDetails() {
+  return $.ajax({
+    method: "GET",
+    url: "/users/me",
+  });
+}
+
+function logOut() {
+  return $.ajax({
+    method: "POST",
+    url: "/users/logout",
+  });
+}
+
+function logIn(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/users/login",
+    data,
+  });
+}
+
+function signUp(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/users",
+    data,
+  });
+}
+
+function getAllListings(params) {
+  let url = "/api/properties";
+  if (params) {
+    url += "?" + params;
+  }
+  return $.ajax({
+    url,
+  });
+}
+
+const submitProperty = function (data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/properties",
+    data,
+  });
+};
+
+const submitReservation = function (data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/reservations",
+    data,
+  });
+};
+
+function getFulfilledReservations() {
+  let url = "/api/reservations";
+  return $.ajax({
+    url,
+  });
+}
+
+function getUpcomingReservations() {
+  let url = "/api/reservations/upcoming";
+  return $.ajax({
+    url,
+  });
+}
+
+function getIndividualReservation(reservationId) {
+  let url = `/api/reservations/${reservationId}`;
+  return $.ajax({
+    url,
+  });
+}
+
+const updateReservation = function (data) {
+  return $.ajax({
+    method: "POST",
+    url: `/api/reservations/${data.reservation_id}`,
+    data,
+  });
+};
+
+const deleteReservation = function (data) {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/reservations/${data}`,
+  });
+};
+
+const getReviewsByProperty = function (propertyId) {
+  const url = `api/reviews/${propertyId}`;
+  return $.ajax({
+    url,
+  });
+};
+
+const submitReview = function (data) {
+  return $.ajax({
+    method: "POST",
+    url: `api/reviews/${data.reservationId}`,
+    data,
+  });
+};
